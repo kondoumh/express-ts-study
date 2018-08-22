bd52fd36869e9b455d096d6f15eeb7407737f0e7
kondoumh <kondoumh@gmail.com>
2018-08-23 06:27:34 +0900

    WIP part3 step2

--- node-apis-project/lib/routes/crmRoutes.ts
+++ node-apis-project/lib/routes/crmRoutes.ts
@@ -1,15 +1,21 @@
 import {Request, Response} from "express";
+import { ContactController } from "../controllers/crmController";
+
 
 export class Routes {
+
+    public contactController: ContactController = new ContactController();
+
     public routes(app) : void {
+
         app.route('/')
         .get((req: Request, res:Response) => {
             res.status(200).send({
                 message: 'Get request successful!!!!'
             })
         })
-        // Contract
-        app.route('/contract')
+        // Contact
+        app.route('/contact')
         // GET endpoint
         .get((req: Request, res: Response) => {
         // Get all contacts
@@ -19,29 +25,32 @@ export class Routes {
         })
         // POST endpoint
         .post((req: Request, res: Response) => {
-        // Create new contract
+        // Create new contact
+            app.route('/contact').post(this.contactController.addNewContact);
             res.status(200).send({
                 message: 'POST request successfull!!'
             })
         })
 
-        // Contract detail
-        app.route('/contract/:contractId')
-        // get specific contract
+        // Contact detail
+        app.route('/contact/:contactId')
+        // get specific contact
         .get((req: Request, res: Response) => {
-        // Get a single contract detail
+        // Get a single contact detail
+            app.route('/contact/:contactId').get(this.contactController.getContactWithId)
             res.status(200).send({
                 message: 'GET request successfull!!!'
             })
         })
         .put((req: Request, res: Response) => {
-        // Update a contract
+        // Update a contact
+
             res.status(200).send({
                 message: 'PUT request sucessfull!!!!'
             })
         })
         .delete((req: Request, res: Response) => {
-        // Delete a contract
+        // Delete a contact
             res.status(200).send({
                 message: 'DELETE request sucessfull!!!!'
             })
