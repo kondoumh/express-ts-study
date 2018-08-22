190f233fcb8000f06db94fca1eab4ada826caa92
kondoumh <kondoumh@gmail.com>
2018-08-23 05:39:20 +0900

    part2 step2

--- node-apis-project/lib/routes/crmRoutes.ts
+++ node-apis-project/lib/routes/crmRoutes.ts
@@ -8,5 +8,43 @@ export class Routes {
                 message: 'Get request successful!!!!'
             })
         })
+        // Contract
+        app.route('/contract')
+        // GET endpoint
+        .get((req: Request, res: Response) => {
+        // Get all contacts
+            res.status(200).send({
+                message: 'GET request sucessfull!!!!'
+            })
+        })
+        // POST endpoint
+        .post((req: Request, res: Response) => {
+        // Create new contract
+            res.status(200).send({
+                message: 'POST request successfull!!'
+            })
+        })
+
+        // Contract detail
+        app.route('/contract/:contractId')
+        // get specific contract
+        .get((req: Request, res: Response) => {
+        // Get a single contract detail
+            res.status(200).send({
+                message: 'GET request successfull!!!'
+            })
+        })
+        .put((req: Request, res: Response) => {
+        // Update a contract
+            res.status(200).send({
+                message: 'PUT request sucessfull!!!!'
+            })
+        })
+        .delete((req: Request, res: Response) => {
+        // Delete a contract
+            res.status(200).send({
+                message: 'DELETE request sucessfull!!!!'
+            })
+        })
     }
 }
